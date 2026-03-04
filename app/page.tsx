7de14ee99119f834894369a13c231c246b5de293
aurelien bernard <aurelien.bernard85@orange.fr>
2026-03-04 16:43:39 +0100

    Update Hello World

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,18 @@ export default function Home() {
       justifyContent: "center",
       alignItems: "center",
       height: "100vh",
-      fontSize: "2rem",
-      fontWeight: "bold",
       textAlign: "center"
     }}>
       <Image
-        className="dark:invert"
         src="/next.svg"
         alt="Next.js logo"
         width={180}
         height={38}
         priority
       />
-      <div style={{ marginTop: "20px" }}>
-        Hello World CI/CD avec Jenkins + Docker + Heroku + mh_lab + ngrok
-      </div>
+      <h1 style={{ marginTop: "20px", fontSize: "24px" }}>
+        Hello World CI/CD avec Jenkins + Docker + Heroku
+      </h1>
     </main>
   );
 }

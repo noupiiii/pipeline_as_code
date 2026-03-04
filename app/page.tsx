66900b180877c74f2c6dcae0bab07f74ccf90005
aurelien bernard <aurelien.bernard85@orange.fr>
2026-03-04 16:35:09 +0100

    MAJ message

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,14 @@ export default function Home() {
   return (
     <main style={{
       display: "flex",
+      flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
       height: "100vh",
-      fontSize: "3rem",
+      fontSize: "2rem",
       fontWeight: "bold",
-      textAlign: "center",
-      flexDirection: "column",
-      gap: "20px"
+      textAlign: "center"
     }}>
-      {/* Affichage du logo Next.js pour faire propre */}
       <Image
         className="dark:invert"
         src="/next.svg"
@@ -22,13 +20,9 @@ export default function Home() {
         height={38}
         priority
       />
-
-      {/* Ton message objectif */}
-      <div>Hello WORLD CI/CD</div>
-
-      <p style={{ fontSize: "1rem", color: "#666" }}>
-        Jenkins + Docker + Heroku + mh_lab + ngrok
-      </p>
+      <div style={{ marginTop: "20px" }}>
+        Hello World CI/CD avec Jenkins + Docker + Heroku + mh_lab + ngrok
+      </div>
     </main>
   );
 }
